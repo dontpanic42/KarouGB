b430acd7468894f12744f8f559aa1fce363f5b85
dontpanic42 <info>
2014-12-02 12:31:32 +0100

    Cartridges können per Kommandozeilenparameter bestimmt werden. Unbenötigten Code entfernt.

--- KarouGB/main.cpp
+++ KarouGB/main.cpp
@@ -17,67 +17,15 @@
 #include "buttons.h"
 #include "timewarp.h"
 
-/* Anzahl der Zyklen, nach der ein delay aktiviert wird */
-//#define DELAY_MEASUR_CYCLE_COUNT 16000
-///* In KHZ, d.h. cycles pro millisekunde */
-//#define DELAY_CPU_SPEED 4194
-
-/* Kommentar entfernen, um die delayroutine zu _deaktivieren_.
- Resultiert in emulation mit voller möglicher geschwindigkeit. */
-//#define ENABLE_FULL_SPEED
-
-//sf::Clock delayClock;
-
-/**
- * Misst die Zeit, die eine Anzahl von CPU-Ops benötigt haben (inkl. Gpu updates etc.).
- Ist die benötigte Zeit < der Zeit auf einem echten GameBoy, wird ein künstliches
- Delay eingefügt (bei 4194 KHZ/200.000 Cycs ~20-30 ms) */
-//void delay(const cpu::Context & c)
-//{
-//    static u32i cycleCountLastDelay = 0;
-//    
-//    u32i delta = c.clock.t - cycleCountLastDelay;
-//    
-//    if(delta >= DELAY_MEASUR_CYCLE_COUNT)
-//    {
-//        cycleCountLastDelay = c.clock.t;
-//        
-//        /* Zeit in MS, die diese Anzahl an zyklen auf dem echten gerät gedauert hätte */
-//        s32i virtualms = (s32i) (delta / DELAY_CPU_SPEED);
-//        /* Zeit in MS, die diese Anzahl an zyklen auf diesem rechner gedauert hat */
-//        s32i actualms = delayClock.getElapsedTime().asMilliseconds();
-//        
-//        
-//        /* Wenn die emulation schneller war, als die virtuelle maschine, sleep() */
-//        /* Sleep ist ok, da bei der derzeitigen Konfiguration (4194KHZ/200000Ops)
-//         Maximal 20-30ms geschlafen wird - das ist ~1 Frame bei einem Spiel... */
-//        if(actualms < virtualms)
-//        {
-//            s32i timeToSleep = virtualms - actualms;
-//            
-//#if defined(POSIX)
-//            /* posix usleep(1000 * ms) */
-//            usleep(timeToSleep * 1000);
-//#elif defined(_WIN32)
-//            /* windows Sleep(ms) */
-//            Sleep(timeToSleep);
-//#endif
-//        }
-//        
-//        /* Time Debug Output */
-//        //actualms = delayClock.getElapsedTime().asMilliseconds();
-//        //std::cout << "Geschwindigkeit: " << (int) ((((float) virtualms) / ((float) actualms) * 100.f)) << "%" << std::endl;
-//        /* Time Debug Output End */
-//        
-//        delayClock.restart();
-//    }
-//}
-
 int main(int argc, const char * argv[])
 {
-    const std::string cartridge = "LegendOfZelda.gb";
-    //const std::string cartridge = "cpu_instrs.gb";
-    //const std::string cartridge = "06-ld r,r.gb";
+    if(argc < 2)
+    {
+        std::printf("Usage: ./KarouGB cartridge_file_to_run.gb\n");
+        return EXIT_SUCCESS;
+    }
+    
+    const std::string cartridge(argv[1]);
     
     std::shared_ptr<IOProvider> ioprovider =  std::make_shared<SDLIOProvider>();
     std::shared_ptr<MMU>        mmu =         std::make_shared<MMU>(cartridge);
@@ -89,7 +37,7 @@ int main(int argc, const char * argv[])
     Debugger                    dbg(cpu, mmu, c);
     Timewarp                    timewarp;
     
-    ioprovider->init("EmuGB++");
+    ioprovider->init("KarouGB++");
     
     while(!ioprovider->isClosed() && dbg.poll())
     {
